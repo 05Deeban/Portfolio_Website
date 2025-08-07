f62ffef892a8e10b591171a1969f8faf40958143
Deeban M <amdeeban@gmail.com>
2025-08-07 21:22:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+/*Project scroll*/
+function scrollLeft() {
+  const container = document.getElementById('scrollable-projects');
+  if (container) {
+    container.scrollBy({ left: -250, behavior: 'smooth' });
+  }
+}
+
+function scrollRight() {
+  const container = document.getElementById('scrollable-projects');
+  if (container) {
+    container.scrollBy({ left: 250, behavior: 'smooth' });
+  }
+}
+/*Mail */
+  document.querySelector(".form").addEventListener("submit", function (event) {
+    event.preventDefault(); // prevent actual form submission
+
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const email = document.getElementById("email").value;
+    const comments = document.getElementById("comments").value;
+
+    const subject = encodeURIComponent("New Message from Contact Form");
+    const body = encodeURIComponent(
+      `First Name: ${fname}\nLast Name: ${lname}\nEmail: ${email}\n\nComments / Questions:\n${comments}`
+    );
+
+    // Replace YOUR_EMAIL with your Gmail address
+    const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=amdeeban@gmail.com&su=${subject}&body=${body}`;
+
+    window.open(gmailLink, "_blank");
+  });
+
+
+
+
